a70e579c2943efec5b5efbc9a83acb74bc5d9718
zfz04 <2609176722@qq.com>
2024-12-07 22:18:33 +0800

    增加上传图片功能

--- file_comment_system/vue/src/views/manager/AI.vue
+++ file_comment_system/vue/src/views/manager/AI.vue
@@ -10,7 +10,7 @@
 
       <el-table :data="data.tableData" stripe>
         <el-table-column prop="name" width="100" label="名称"/>
-        <el-table-column prop="logo" width="100" label="logo">
+        <el-table-column prop="cover" width="100" label="logo">
           <template #default="scope">
             <el-image :src="scope.row.logo" style="width: 40px; height: 40px; border-radius: 5px"
                       :preview-src-list="[scope.row.logo]" preview-teleported></el-image>
@@ -46,16 +46,25 @@
         <el-form-item label="名称">
           <el-input v-model="data.form.name" autocomplete="off" placeholder="请输入名称"/>
         </el-form-item>
-        <el-form-item label="logo" prop="logo">
-          <el-upload :action="uploadUrl" list-type="picture" :on-change="handleLogo">
-            <el-button type="primary">上传logo</el-button>
-          </el-upload>
+        <el-form-item label="logo">
+          <input type="file" id="logo" @change="handleLogo" class="form-input" ref="logoInput"
+                 style="display: none;"/>
+          <el-button type="primary" @click="triggerLogoInput">
+            上传logo
+          </el-button>
         </el-form-item>
-        <el-form-item label="封面" prop="cover">
-          <el-upload list-type="picture" :on-change="handleImg">
-            <el-button type="primary">上传封面</el-button>
-          </el-upload>
+        <el-image v-if="data.newLogoUrl||data.form.logo" :src="data.newLogoUrl ? data.newLogoUrl : data.form.logo"
+                  style="width: 90px; height: 90px; border-radius: 5px; margin-left: 80px; margin-bottom: 10px; margin-top: -10px"></el-image>
+        <el-form-item label="封面">
+          <input type="file" id="cover" @change="handleCover" class="form-input" ref="imgInput"
+                 style="display: none;"/>
+          <el-button type="primary" @click="triggerCoverInput">
+            上传封面
+          </el-button>
         </el-form-item>
+        <el-image v-if="data.newLogoUrl||data.form.cover" :src="data.newCoverUrl ? data.newCoverUrl : data.form.cover"
+                  style="width: 160px; height: 90px; border-radius: 5px; margin-left: 80px; margin-bottom: 10px; margin-top: -10px"></el-image>
+
         <el-form-item label="分类">
           <el-dropdown split-button type="primary">
             <div v-if="!data.newTypeName">选择分类</div>
@@ -99,7 +108,6 @@ import {ElMessage, ElMessageBox} from "element-plus";
 
 // 文件上传的接口地址
 const uploadUrl = import.meta.env.VITE_BASE_URL + '/entity-ai/'
-
 const data = reactive({
   tableData: [],
   total: 0,
@@ -112,6 +120,8 @@ const data = reactive({
   newTypeName: null,
   newCover: null,
   newLogo: null,
+  newLogoUrl: null,
+  newCoverUrl: null
 })
 
 const load = () => {
@@ -139,20 +149,54 @@ const handleAdd = () => {
   data.newCover = null
   data.newLogo = null
   data.newTypeName = null
+  data.newCoverUrl = null
+  data.newLogoUrl = null
   data.formVisible = true
 }
 
-const add = () => {
-  request.post('/entity-ai/', data.form).then(res => {
+const add = async () => {
+  await request.post('/entity-ai/', data.form).then(res => {
     if (res.status === 'success') {
       console.log(data.form)
-      load()
+      data.form.id = res.data.id
       data.formVisible = false
-      ElMessage.success('操作成功')
     } else {
       ElMessage.error(res.msg)
+      return
     }
   })
+
+  if (data.newCover || data.newLogo) {
+    const imgFormData = new FormData()
+    if (data.newCover) imgFormData.append('cover', data.newCover)
+    if (data.newLogo) imgFormData.append('logo', data.newLogo)
+    await request({
+      url: '/entity-ai/' + data.form.id + '/',
+      method: 'patch',
+      data: imgFormData,
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(res => {
+      if (!res.status === 'success') {
+        ElMessage.error(res.msg)
+        return
+      }
+    })
+  }
+  load()
+  handleAdd()
+  ElMessage.success('操作成功')
+}
+
+const triggerLogoInput = () => {
+  const LogoInput = document.getElementById('logo')
+  LogoInput.click()// 点击隐藏的 input 元素
+}
+
+const triggerCoverInput = () => {
+  const CoverInput = document.getElementById('cover')
+  CoverInput.click()// 点击隐藏的 input 元素
 }
 
 // 编辑按钮触发
@@ -162,16 +206,48 @@ const handleEdit = (row) => {
   data.formVisible = true
   // console.log(data.form)
 }
-const update = () => {
-  request.patch('/entity-ai/' + data.form.id + '/', data.form).then(res => {
-    if (res.status === 'success') {
-      load()
-      data.formVisible = false
-      ElMessage.success('操作成功')
-    } else {
-      ElMessage.error(res.msg)
+const update = async () => {
+  //console.log(data.form)
+  const aiUpadteResponse = await request({
+    url: '/entity-ai/' + data.form.id + '/',
+    method: 'patch',
+    data: {
+      name: data.form.name,
+      description: data.form.description,
+      url: data.form.url,
+      type_id: data.form.type_id,
     }
   })
+
+  if (!(aiUpadteResponse.status === 'success')) {
+    ElMessage.error(aiUpadteResponse.msg)
+    return
+  }
+  if (data.newCover || data.newLogo) {
+    const imgFormData = new FormData()
+    if (data.newCover) imgFormData.append('cover', data.newCover)
+    if (data.newLogo) imgFormData.append('logo', data.newLogo)
+    await request({
+      url: '/entity-ai/' + data.form.id + '/',
+      method: 'patch',
+      data: imgFormData,
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(res => {
+      if (!res.status === 'success') {
+        ElMessage.error(res.msg)
+        return
+      }
+    })
+  }
+
+  data.newLogoUrl = null
+  data.newCoverUrl = null
+  data.formVisible = false
+  ElMessage.success('操作成功')
+  load()
+
 }
 
 const save = () => {
@@ -193,13 +269,20 @@ const del = (id) => {
   })
 }
 
-const handleImg = (res) => {
-  data.newCover = res.data  // res.data就是文件上传返回的文件路径，获取到路径后赋值表单的属性
+const handleCover = (event) => {
+  const file = event.target.files[0]
+  data.newCover = file
+  data.newCoverUrl = URL.createObjectURL(file)
 }
 
-const handleLogo = (file) => {
-  data.newLogo = file  // res.data就是文件上传返回的文件路径，获取到路径后赋值表单的属性
-  data.prevNewLogo = URL.createObjectURL(file.raw)
+const handleLogo = (event) => {
+  //console.log(file)
+  const file = event.target.files[0]
+  data.newLogo = file
+  data.newLogoUrl = URL.createObjectURL(file)
+  console.log(data.newLogo)
+  console.log(data.newLogoUrl)
+  // console.log(URL.createObjectURL(file))
 }
 
 //获取TypeList
@@ -210,4 +293,19 @@ const handleDropdownItemClick = (res) => {
   data.form.type_id = res.id
   data.newTypeName = res.name
 }
+
+const handleImageError = () => {
+  console.log(data.newLogoUrl);
+}
 </script>
+
+<style scoped>
+.logo {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  cursor: pointer;
+  object-fit: cover;
+  margin-bottom: 15px;
+}
+</style>
